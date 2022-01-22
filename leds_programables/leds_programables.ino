a53103ef65dd13cbf50c2a12ca2662468fd3a6a3
Carlos Fernandez Ruiz <carlos.fernandez.ruiz@estudiantat.upc.edu>
2022-01-22 10:53:50 +0100

    leds programables

--- leds_programables/leds_programables.ino
+++ leds_programables/leds_programables.ino
@@ -1,7 +1,7 @@
 /*
- * Adafruit neopixel:
- * - https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
- */
+   Adafruit neopixel:
+   - https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
+*/
 
 #include <Adafruit_NeoPixel.h>
 
@@ -10,7 +10,11 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 10
+#define LED_COUNT 189
+
+#define TIEMPO_TOTAL  30.0 // minutos
+#define TIEMPO_MS     TIEMPO_TOTAL*60.0*1000.0 // ms
+#define DLY_POR_LED   TIEMPO_MS / LED_COUNT
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -23,25 +27,35 @@ Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
+uint32_t red = pixels.Color(255, 0, 0, 150);
 void setup() {
+  Serial.begin(115200);
+  Serial.print("Tiempo Total [ms]: "); Serial.println(TIEMPO_MS);
+  Serial.print("Tiempo delay [ms]: "); Serial.println(DLY_POR_LED);
+  
   pixels.begin();
+  delay(1000);
+  pixels.clear();
+  pixels.fill(red);
   pixels.show(); // Initialize all pixels to 'off'
+
+
 }
 
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  //  pixels.clear(); // Set all pixel colors to 'off'
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
-  for(int i=0; i<LED_COUNT; i++) { // For each pixel...
+  for (int i = 0; i < LED_COUNT; i++) { // For each pixel...
+
+    delay(DLY_POR_LED); // Pause before next pass through loop
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.setPixelColor(i, 0, 0, 0, 0);
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(500); // Pause before next pass through loop
+    pixels.show();   // Send the updated pixel colors to the hardware. 
   }
 }
