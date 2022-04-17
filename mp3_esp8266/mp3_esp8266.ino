73a08e8082818b591a84bbd78ab020c5d6afcf93
Carlos Fernandez <carlos.fernandez.ruiz@estudiantat.upc.edu>
2022-04-17 18:40:53 +0200

    upload/compile comand

--- mp3_esp8266/mp3_esp8266.ino
+++ mp3_esp8266/mp3_esp8266.ino
@@ -1,16 +1,21 @@
 /*
- CONTROLAR MP3 con nodmecu y web
+ * CONTROLAR MP3 con nodmecu y web
+ *
+ * //play specific mp3 in SD:/MP3/0004.mp3; File Name(0~65535)
+ * 192.168.1.40/play?pista=2&vol=30
+ * 192.168.1.40/stop
+ *
+ * Compile:
+ *  arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 mp3_esp8266
+ * Upload:
+ *  arduino-cli upload -p /dev/ttyUSB0 --fqbn esp8266:esp8266:nodemcuv2 mp3_esp8266
+ *
+ */
 
- //play specific mp3 in SD:/MP3/0004.mp3; File Name(0~65535)
- 192.168.1.40/play?pista=2&vol=30
- 192.168.1.40/stop
-
-*/
-
-#define DEBUG     1
+#define DEBUG    1
 #if DEBUG == 1
-#define LOGN(x) Serial.println(x)
-#define LOG(x)  Serial.print(x)
+#define LOGN(x)    Serial.println(x)
+#define LOG(x)     Serial.print(x)
 #else
 #define LOG(x)
 #define LOGN(x)
@@ -33,15 +38,15 @@ const char *password = "6162909297";
 ESP8266WebServer server(80);
 
 // ==== MP3 ======================================================
-#define SS_RX D2
-#define SS_TX D3
-SoftwareSerial mySoftwareSerial(SS_RX, SS_TX); // RX, TX
+#define SS_RX    D2
+#define SS_TX    D3
+SoftwareSerial      mySoftwareSerial(SS_RX, SS_TX); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
 
 // ==== GPIO =====================================================
-#define PIN_LEDS  D5
-#define LED_ON    HIGH
+#define PIN_LEDS    D5
+#define LED_ON      HIGH
 bool led_state;
 
 
@@ -53,14 +58,14 @@ void setup()
 {
   ///////////////////////
   // -- Serial --
-  #if DEBUG == 1
-    Serial.begin(115200);
-    while (!Serial) {
-      ;
-    }
-    delay(50);
-    LOGN("\n\n---------------\n  EMPEZAMOS");
-  #endif
+#if DEBUG == 1
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
+  delay(50);
+  LOGN("\n\n---------------\n  EMPEZAMOS");
+#endif
 
   ///////////
   // Leds
@@ -70,11 +75,14 @@ void setup()
   ///////////////
   // myDFPlayer
   mySoftwareSerial.begin(9600);
-  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+  if (!myDFPlayer.begin(mySoftwareSerial))    //Use softwareSerial to communicate with mp3.
+  {
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-  } else {
+  }
+  else
+  {
     Serial.println(F("DFPlayer Mini online."));
   }
 
@@ -93,6 +101,7 @@ void setup()
 
   server.begin();
 }
+
 
 void setup_wifi()
 {
@@ -133,10 +142,12 @@ void loop()
   server.handleClient();
 
   // Read State of myDFPlayer
-  if (myDFPlayer.available()) {
+  if (myDFPlayer.available())
+  {
     handleDFPlayer();
   }
 }
+
 
 // === GPIOS =====================================================
 void setLed(bool ledVal)
@@ -145,99 +156,117 @@ void setLed(bool ledVal)
   led_state = ledVal;
 }
 
+
 // ===============================================================
 //            MP3
 // ===============================================================
 void handleDFPlayer()
 {
   // Read type and value
-  uint8_t type = myDFPlayer.readType();
-  int value = myDFPlayer.read();
+  uint8_t type  = myDFPlayer.readType();
+  int     value = myDFPlayer.read();
 
   // handle cases we care about!
-  switch(type)
+  switch (type)
   {
-    // FINISHED PLAYING !!
-    case DFPlayerPlayFinished:
-      setLed(false);
-      break;
+  // FINISHED PLAYING !!
+  case DFPlayerPlayFinished:
+    setLed(false);
+    break;
 
-    default:
-      break;
+  default:
+    break;
   }
 
   // Print State!
-  #if DEBUG == 1
+#if DEBUG == 1
   printmyDFPlayerDetail(type, value);
-
-  #endif
+#endif
 }
 
 
 ///////////////////////////////////////////////////////
 // print details of MP3
-void printmyDFPlayerDetail(uint8_t type, int value){
+void printmyDFPlayerDetail(uint8_t type, int value)
+{
   Serial.print("myMp3:   ");
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
+  switch (type)
+  {
+  case TimeOut:
+    Serial.println(F("Time Out!"));
+    break;
+
+  case WrongStack:
+    Serial.println(F("Stack Wrong!"));
+    break;
+
+  case DFPlayerCardInserted:
+    Serial.println(F("Card Inserted!"));
+    break;
+
+  case DFPlayerCardRemoved:
+    Serial.println(F("Card Removed!"));
+    break;
+
+  case DFPlayerCardOnline:
+    Serial.println(F("Card Online!"));
+    break;
+
+  case DFPlayerPlayFinished:
+    Serial.print(F("Number:"));
+    Serial.print(value);
+    Serial.println(F(" Play Finished!"));
+    break;
+
+  case DFPlayerError:
+    Serial.print(F("DFPlayerError:"));
+    switch (value)
+    {
+    case Busy:
+      Serial.println(F("Card not found"));
       break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
+
+    case Sleeping:
+      Serial.println(F("Sleeping"));
       break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
+
+    case SerialWrongStack:
+      Serial.println(F("Get Wrong Stack"));
       break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
+
+    case CheckSumNotMatch:
+      Serial.println(F("Check Sum Not Match"));
       break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
+
+    case FileIndexOut:
+      Serial.println(F("File Index Out of Bound"));
       break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
+
+    case FileMismatch:
+      Serial.println(F("Cannot Find File"));
       break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
+
+    case Advertise:
+      Serial.println(F("In Advertise"));
       break;
+
     default:
       break;
-  }
+    }
+    break;
 
+  default:
+    break;
+  }
 }
+
 
 // ===============================================================
 //            SERVER CALLBACKS
 // ===============================================================
 // --- on reload ---
-void handleRoot() {
+void handleRoot()
+{
   digitalWrite(led, 1);
   server.send(200, "text/plain", html_string);
   digitalWrite(led, 0);
@@ -254,7 +283,9 @@ void handlePlay()
   {
     LOGN("Parametro 'pista' no encontrado");
     return;
-  } else {
+  }
+  else
+  {
     String pista_str = server.arg("pista");
     pista = pista_str.toInt();
   }
@@ -263,7 +294,9 @@ void handlePlay()
   {
     vol = 25;
     LOGN("No hay parametro de volumen!");
-  } else {
+  }
+  else
+  {
     String vol_str = server.arg("vol");
     vol = vol_str.toInt();
   }
@@ -276,16 +309,18 @@ void handlePlay()
   setLed(true);
 }
 
+
 void handleStop()
 {
   // Stop!
   setLed(false);
-  myDFPlayer.pause();  //pause the mp3  
+  myDFPlayer.pause();  //pause the mp3
 }
 
 
 // --- on handle not found ---
-void handleNotFound() {
+void handleNotFound()
+{
   digitalWrite(led, 1);
   String message = "File Not Found\n\n";
   message += "URI: ";
@@ -295,7 +330,10 @@ void handleNotFound() {
   message += "\nArguments: ";
   message += server.args();
   message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) { message += " " + server.argName(i) + ": " + server.arg(i) + "\n"; }
+  for (uint8_t i = 0; i < server.args(); i++)
+  {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
   server.send(404, "text/plain", message);
   digitalWrite(led, 0);
 }
